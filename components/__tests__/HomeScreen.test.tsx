efc870c626c73006a9cc6071b139d588a415b804
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-01-08 20:50:57 +0100

    test: ran simple jest test

--- components/__tests__/HomeScreen.test.tsx
+++ components/__tests__/HomeScreen.test.tsx
@@ -3,6 +3,11 @@ import { render } from '@testing-library/react-native';
 import HomeScreen, { CustomText } from '@/app/index';
 
 describe('<HomeScreen />', () => {
+  test('CustomText renders correctly', () => {
+    const tree = render(<CustomText>Some text</CustomText>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
   test('Text renders correctly on HomeScreen', () => {
     const { getByText } = render(<HomeScreen />);
 
