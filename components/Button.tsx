28cde277885ebb9ee77d71f3ca392e5d4753081e
Kelechi Orunta <kelechiorunta1@gmail.com>
2026-01-17 14:21:49 +0100

    feat: fix optional types in intersection type

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,6 +1,14 @@
-import { View, Text, ButtonProps, Pressable, StyleProp, PressableProps } from 'react-native';
+import {
+  View,
+  Text,
+  ButtonProps,
+  Pressable,
+  StyleProp,
+  PressableProps,
+  StyleSheet
+} from 'react-native';
 
-type CustomButtonTypes = { id: string; name: string; styles: Object };
+type CustomButtonTypes = { id?: string; name?: string; styles?: Object };
 
 /**
  * This is an intersection type that merges a custom button type with the native PressableProps
@@ -14,16 +22,25 @@ const Button = ({ id, name, testID, onPress, styles }: customButtonProps) => {
   const returnID = (): buttonID => {
     return '';
   };
+
+  const buttonStyles = { ...styles, ...currentStyles.container };
   return (
     <Pressable
       onPress={onPress}
       testID={testID}
       id={id}
-      style={styles as StyleProp<PressableProps>}
+      style={buttonStyles as StyleProp<PressableProps>}
     >
       <Text>{name}</Text>
     </Pressable>
   );
 };
+
+const currentStyles = StyleSheet.create({
+  container: {
+    padding: 10,
+    borderRadius: 10
+  }
+});
 
 export default Button;
